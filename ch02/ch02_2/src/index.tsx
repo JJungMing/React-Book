5226b2626cac474e16e276d2611ae93e335119ee
JJungMing <1010pjm@naver.com>
2026-03-02 16:52:05 +0900

    ch02 JSX code push

---
+++ ch02/ch02_2/src/index.tsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom/client'
+import * as D from './data'
+import reportWebVitals from './reportWebVitals'
+
+const children = D.makeArray(10).map((notUsed, index) => (
+  <div key={index}>
+    <p>{D.randomId()}</p>
+    <p>{D.randomName()}</p>
+    <p>{D.randomJobTitle()}</p>
+    <p>{D.randomSentence()}</p>
+    <img src={D.randomAvatar()} width={100} height={100} />
+  </div>
+))
+
+const rootVirtualDOM = <div>{children}</div>
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(rootVirtualDOM)
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
