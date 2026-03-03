bbcd260987aaee03ced27cea606f408bfbad3a9e
JJungMing <1010pjm@naver.com>
2026-03-03 14:54:59 +0900

    Componet code

---
+++ ch02/ch02_3/src/App.tsx
@@ -0,0 +1,11 @@
+import ClassComponent from './ClassComponet'
+import ArrowComponent from './ArrowComponet'
+
+export default function App() {
+  return (
+    <div>
+      <ClassComponent href="https://www.google.com" text="Google" />
+      <ArrowComponent href="https://www.facebook.com" text="Facebook" />
+    </div>
+  )
+}
