06e55b132118ac367b9557506db12162a191a7c8
JJungMing <1010pjm@naver.com>
2026-04-12 18:13:02 +0900

    useEffect and useLayoutEffect hook

---
+++ ch04/ch04_4/src/App.tsx
@@ -0,0 +1,13 @@
+import ClassLifecycle from './pages/ClassLifecycle'
+import WindowResizeTest from './pages/WindowResizeTest'
+import FetchTest from './pages/FetchTest'
+
+export default function App() {
+  return (
+    <div>
+      <FetchTest />
+      <WindowResizeTest />
+      <ClassLifecycle />
+    </div>
+  )
+}
